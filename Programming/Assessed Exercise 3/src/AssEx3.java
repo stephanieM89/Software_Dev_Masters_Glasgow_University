00a7753fa03db1bd51f8058ba5f01a08df0abb10
stephanieM89 <31744964+stephanieM89@users.noreply.github.com>
2018-08-05 02:03:02 +0100

    Add files via upload

---
+++ Programming/Assessed Exercise 3/src/AssEx3.java
@@ -0,0 +1,16 @@
+/**
+ * The main class
+ */
+public class AssEx3
+{
+    /**
+     * The main method
+     * @param args the arguments
+     */
+    public static void main(String[] args)
+    {
+        SportsCentreGUI display = new SportsCentreGUI();
+        display.setVisible(true);
+   
+    }
+}
